e211eb8f6cb7fffdcc023c8b12eb97a1b2ff5e66
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-03-15 11:40:08 +0100

    He actualizado lo del control de errores, aunque me queda ponerlo mejor, pero ya no admite la misma moneda al cambio, ni el valor de 0, ni negativo de la cantidad.

--- criptoapp/routes.py
+++ criptoapp/routes.py
@@ -1,5 +1,5 @@
 from criptoapp import app
-from flask import render_template
+from flask import render_template, request, flash, redirect
 from models import select_all
 
 @app.route("/")
@@ -7,9 +7,19 @@ def inicio():
     movimientos = select_all()
     return render_template("index.html", datos=movimientos)
 
-@app.route("/compras")
+@app.route("/compras", methods=['GET','POST'])
 def compra():
+    if request.method == 'POST':
+        moneda_venta = request.form.get('from_currency')
+        moneda_compra = request.form.get('to_currency')
+
+        if moneda_venta == moneda_compra:
+            print("Error: No puedes cambiar la misma moneda")
+            return render_template("compras.html", error="Las monedas no pueden ser iguales.")
+        return redirect("/")
+        
     return render_template("compras.html")
+            
 
 @app.route("/cartera")
 def cartera():
